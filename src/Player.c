29484d69590d7ba5d0646522e9d31e36cb73c083
fosong98 <fosong98@gmail.com>
2022-05-30 10:07:54 +0900

    <feat> : Add Player.c to make music player
    * 작성된 라이브러리들을 사용하여 뮤직 플레이어를 구현합니다.

---
+++ src/Player.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <../include/linkedlist.h>
+#include <../include/textfilewriter.h>
+#include <Player.h>
+#include <string.h>
+
+static Node* cur;
+
+int main() {
+	int n;
+	int i = 0;
+	int temp;
+	char buffer[50];
+
+	scanf("%d", &n);
+
+	for (; i < n; ++i) {
+		if (1 == scanf("%s", buffer) {
+			append_left(strlen(buffer) + 1, buffer);
+		}
+	}
+
+	cur = first();
+
+	scanf("%d", n);
+
+
+	for (i = 0; i < n; ++i) {
+		if (1 != scanf("%s", buffer)) {
+			printf("Empty!!!\n");
+			break;
+		}
+		
+		if (strcmp(buffer, "add")) {
+			if (1 == scanf("%s", buffer)) {
+				add(buffer);
+		       	}	
+		}
+		else if (strcmp(buffer, "del") {
+			if (1 == scanf("%s", buffer)) {
+				del(buffer);
+			}
+		}
+		else if (strcmp(buffer, "list") {
+			list();
+		}
+		else if (strcmp(buffer, "next") {
+			next_node();
+		}
+		else if (strcmp(buffer, "prev") {
+			prev_node();
+		}
+		else if (strcmp(buffer, "move") {
+			if (1 == scanf("%d", &temp)) {
+				move(temp);
+			}
+		}
+		else if (strcmp(buffer, "play") {
+			play();
+		}
+		else if (strcmp(buffer, "load") {
+			if (1 == scanf("%s", buffer)) {
+				load(buffer);
+			}
+		}
+		else if (strcmp(buffer, "save") {
+			if (1 == scanf("%s", buffer)) {
+				save(buffer);
+			}
+		}
+		else {
+			printf("Invalid command\n");
+		}
+
+	}	
+}
+
+void add(char* title) {
+	append_left(strlen(title) + 1, title);
+}
+
+void del(char* title) {
+	delete(title);
+}
+
+void list() {
+	print();
+}
+
+void next_node() {
+	cur = next();
+}
+
+void prev_node() {
+	cur = prev();
+}
+
+void move(int index) {
+	insert_after(get_node(index), cur);
+}
+
+void play() {
+	printf("%s is now playing!\n", cur->data);
+}
+
+void load(char* filename) {
+	read_file(filename);
+}
+
+void save(char* filename) {
+	write_file(filename);
+}
+
