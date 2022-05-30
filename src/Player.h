c1052f0c3209b959fb8796a5bc95d7fbc8677a5a
fosong98 <fosong98@gmail.com>
2022-05-30 10:07:54 +0900

    <feat> : Add Player.c to make music player(1)
    * 작성된 라이브러리들을 사용하여 뮤직 플레이어를 구현합니다.

---
+++ src/Player.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void add(char* title);
+
+void del(char* title);
+
+void list();
+
+void next();
+
+void prev();
+
+void move(int index);
+
+void play();
+
+void load(char* filename);
+
+void save(char* filename);
